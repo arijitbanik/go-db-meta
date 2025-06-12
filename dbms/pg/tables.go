4f03bf73c11950e19307d79a9d719b1bc78d8b21
arijitbanik <68817941+arijitbanik@users.noreply.github.com>
2025-06-12 14:31:38 +0530

    Update tables.go

--- dbms/pg/tables.go
+++ dbms/pg/tables.go
@@ -3,7 +3,7 @@ package pg
 import (
 	"database/sql"
 
-	m "github.com/gsiems/go-db-meta/model"
+	m "github.com/arijitbanik/go-db-meta/model"
 )
 
 // Tables defines the query for obtaining a list of tables and views
