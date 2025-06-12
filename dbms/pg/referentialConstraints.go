80096ace7acf32a4a49359026ec61244d27c4817
arijitbanik <68817941+arijitbanik@users.noreply.github.com>
2025-06-12 14:31:01 +0530

    Update referentialConstraints.go

--- dbms/pg/referentialConstraints.go
+++ dbms/pg/referentialConstraints.go
@@ -3,7 +3,7 @@ package pg
 import (
 	"database/sql"
 
-	m "github.com/gsiems/go-db-meta/model"
+	m "github.com/arijitbanik/go-db-meta/model"
 )
 
 // ReferentialConstraints defines the query for obtaining the
